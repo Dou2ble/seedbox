7c02f2b61e5ef817c92af54bd36df22d8df492e6
Dou2ble <mejkstar1234@gmail.com>
2022-09-25 15:14:17 +0200

    better

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from termcolor import colored
 from dotenv import load_dotenv
 import os
 import configparser
+import io
+import tarfile
+import pathlib
 
 import utils
 
@@ -14,7 +17,7 @@ load_dotenv()
 QB_PASSWORD = os.getenv("QB_PASSWORD")
 
 app = Flask(__name__)
-CORS()
+CORS(app)
 
 
 torrent_path = "/home/otto/Skrivbord/Python/flask-seedbox2/torrents"
@@ -23,6 +26,7 @@ qb = Client("http://localhost:8080/")
 qb.login("admin", QB_PASSWORD)
 
 print(colored(qb.torrents(), "green"))
+print(colored(qb.torrents(filter=True), "red"))
 
 #qb.download_from_link(magnet, savepath=torrent_path)
 
@@ -30,12 +34,34 @@ print(colored(qb.torrents(), "green"))
 @app.route("/main")
 @app.route("/index")
 def home():
-    return render_template("index.html", torrents=qb.torrents())
+    torrents = qb.torrents()
+
+    for torrent in torrents:
+        torrent["ratio"] = round(torrent["ratio"], 2)
+        torrent["size"] = utils.sizeof_fmt(torrent["size"])
+        if os.path.isfile(torrent["content_path"]):
+            torrent["filetype"] = torrent["content_path"].split(".")[-1]
+        else:
+            torrent["filetype"] = "folder"
+        torrent["content_path"] = torrent["content_path"].replace(os.getcwd() + "/torrents/", "")
+
+    return render_template("index.html", torrents=torrents)
 
 @app.route("/download/<path:path>")
 def download(path):
-    full_path = "torrents/"
-    return send_file(os.getcwd()+"/torrents/" + path, as_attachment=True)
+    if os.path.isfile("torrents/" + path):
+        return send_file(os.getcwd()+"/torrents/" + path, as_attachment=True)
+    else:
+        with tarfile.open("output/files.tar", "w") as f:
+            for file in os.listdir("torrents/" + path):
+                f.add("torrents/" + path + "/" + file)
+
+        return send_file(
+            os.getcwd() + "/output/files.tar",
+            as_attachment=False,
+            download_filename=".tar"
+        )
+        
 
 @app.route("/resume/<path:path>")
 def resume(path):
@@ -54,4 +80,4 @@ def pause(path):
         qb.pause(path)
     return ('', 204)
 
-app.run(debug=True)
+app.run(host="0.0.0.0", debug=True)
